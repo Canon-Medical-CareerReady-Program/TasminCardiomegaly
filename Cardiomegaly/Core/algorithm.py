beccf599ee524f5f7d58e56aad3ca54adfcd8fa6
Tasmin Jones <Tasmin.jones@icloud.com>
2023-07-04 15:44:37 +0100

    distance formula

--- Cardiomegaly/Core/algorithm.py
+++ Cardiomegaly/Core/algorithm.py
@@ -2,8 +2,8 @@ from math import sqrt
 
 def distance(x1 : float, y1 : float, x2 : float, y2 : float) -> float:
     """ Calculate euclidean distance between two points """
-    print("TODO: calculate distance")
-    return 0
+    distance = sqrt ((x2-x1)**2) + ((y2-y1)**2)
+    return distance
 
 def calculate_ratio(heart_length: float, thorax_length: float) -> float:
     """ Calculate ratio between heart and thorax """
